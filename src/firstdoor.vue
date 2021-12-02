ff11ac8734df4bd6bcc723d00df9e9b7335b3f5a
novb1492 <novb1492@naver.com>
2021-12-02 23:29:18 +0900

    마커생성

--- src/firstdoor.vue
+++ src/firstdoor.vue
@@ -50,7 +50,7 @@ export default {
                 // LatLngBounds 객체에 좌표를 추가합니다
                 var bounds = new kakao.maps.LatLngBounds();
                 for (var i=0; i<result.length; i++) {
-                    //displayMarker(result[i]);    
+                    this.displayMarker(result[i]);    
                     bounds.extend(new kakao.maps.LatLng(result[i].y, result[i].x));
                 }       
 
@@ -61,6 +61,20 @@ export default {
         });
         
     },
+    displayMarker(place) {
+        // 마커를 생성하고 지도에 표시합니다
+        var marker = new kakao.maps.Marker({
+            map: this.map,
+            position: new kakao.maps.LatLng(place.y, place.x) 
+        });
+        var infowindow = new kakao.maps.InfoWindow({zIndex:1});
+        // 마커에 클릭이벤트를 등록합니다
+        kakao.maps.event.addListener(marker, 'click', function() {
+            // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+            infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+            infowindow.open(this.map, marker);
+        });
+    } 
   
   },
  
