9a03b299fd7f9e873658509f14a18d2b2bc80fa9
novb1492 <novb1492@naver.com>
2021-12-02 23:24:37 +0900

    대박이다 이렇게 하면 결과가 있다면 주소도 이동된다..

--- README.md
+++ README.md
@@ -22,3 +22,11 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+생각해논 어플 구상전  
+지도를 사용해야 해서 테스트중입니다  
+
+주의 검색후 지도를 자동으로 움직이고 싶다면  
+//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=95292156744ab5c8586460536149fb32&libraries=services  
+맨뒤에 &libraries=services  를 꼭 붙혀야한다  
--- src/firstdoor.vue
+++ src/firstdoor.vue
@@ -1,5 +1,10 @@
 <template>
-    <div id="map"> </div>
+    <div id="mapPage">
+        <div id="map"></div>
+        <input type="text" @keyup="search()" id="name">
+    </div>
+   
+    
 </template>
 <style scoped>
 #map {
@@ -20,7 +25,7 @@ export default {
       /* global kakao */
       script.onload = () => kakao.maps.load(this.initMap);
       script.src =
-        "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=95292156744ab5c8586460536149fb32";
+        "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=95292156744ab5c8586460536149fb32&libraries=services";
       document.head.appendChild(script);
     
   },
@@ -33,7 +38,30 @@ export default {
       };
       this.map = new kakao.maps.Map(container, options);
     },
-    
+    search(){
+        var n=document.getElementById('name').value;
+        console.log(n);
+        // 장소 검색 객체를 생성합니다
+        var ps = new kakao.maps.services.Places();
+        // 키워드로 장소를 검색합니다
+        ps.keywordSearch(n, (result,status)=>{
+            if (status === kakao.maps.services.Status.OK) {
+                // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+                // LatLngBounds 객체에 좌표를 추가합니다
+                var bounds = new kakao.maps.LatLngBounds();
+                for (var i=0; i<result.length; i++) {
+                    //displayMarker(result[i]);    
+                    bounds.extend(new kakao.maps.LatLng(result[i].y, result[i].x));
+                }       
+
+                // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+                this.map.setBounds(bounds);
+            } 
+
+        });
+        
+    },
+  
   },
  
    
